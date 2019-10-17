b8775259278dca81d17e03f36356ed2e67279264
Sophie <sophie.waltonsmith@gmail.com>
2019-10-17 16:13:11 +0100

    Added static posts

---
+++ src/components/Post.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3>{{post.title}}</h3>
+        <p>{{post.body}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Post',
+        props: ['post']
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Posts.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div v-for="post in posts" v-bind:key="post.id">
+            <Post v-bind:post="post"/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Post from './Post';
+    export default {
+        name: 'Posts',
+        components: {
+            Post
+        },
+        props: ['posts']
+    }
+</script>
+
+<style scoped>
+
+</style>
